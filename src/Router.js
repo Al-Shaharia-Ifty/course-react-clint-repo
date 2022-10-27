5f50a309af70eef2ec54312ce92a5528b25b8508
Al Shaharia <ifty682002@gmail.com>
2022-10-27 12:57:33 +0600

    Course.jsx create

--- src/Router.js
+++ src/Router.js
@@ -1,9 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
+import Course from "./Components/Course";
 import Main from "./Shared/Main";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Course />,
+      },
+    ],
   },
 ]);
