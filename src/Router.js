1a198196355a1e6bd321ac20f326448ff728812b
Al Shaharia <ifty682002@gmail.com>
2022-10-27 13:12:29 +0600

    faq.jsx create

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "./Authentication/Login";
 import Register from "./Authentication/Register";
 import Course from "./Components/Course";
+import FAQ from "./Components/FAQ";
 import Main from "./Shared/Main";
 
 export const router = createBrowserRouter([
@@ -16,6 +17,10 @@ export const router = createBrowserRouter([
       {
         path: "course",
         element: <Course />,
+      },
+      {
+        path: "faq",
+        element: <FAQ />,
       },
       {
         path: "login",
